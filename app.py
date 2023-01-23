23c39d16c4d9513e28a205137447ee02e8785a43
julien-guilet <guiletjulien@gmail.com>
2023-01-23 16:43:29 +0100

    new cov

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 from bottle import route, run, template
-
-
-@route("/")
-def home():
-    return "<b>Homepage</b>!"
+from misc import add
 
 
 @route("/hello/<name>")
@@ -11,33 +7,15 @@ def index(name):
     return template("<b>Hello {{name}}</b>!", name=name)
 
 
+@route("/")
+def homepage():
+    return "<b>Hello you</b>!"
+
+
 @route("/add/<a>/<b>")
-def add(a, b):
-    result = addCalc(a, b)
-    return {"result": result}
-
-
-def addCalc(a, b):
-    """
-        paramètre a : (int) un entier
-        paramètre b : (int) un entier
-        valeur renvoyée : (int) la somme de a et b.
-    Exemples :
-    >>> addCalc(3, 4)
-    7
-
-    >>> addCalc(10, 5)
-    15
-
-    >>> addCalc(-10, 5)
-    -5
-    """
-    return int(a) + int(b)
+@route("/add/<a>/<b>/")
+def route_add(a, b):
+    return {"result": add(a, b)}
 
 
 run(host="localhost", port=8080, reloader=True)
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
